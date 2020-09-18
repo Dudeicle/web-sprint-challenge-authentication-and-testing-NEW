bfa594b995324c1f56eaf255538d4c248391cf98
Dudeicle <brian-griffiths@lambdastudents.com>
2020-09-18 12:09:46 -0400

    everyting prior to testing is up and running successfully, starting tests now

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,77 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-router.post('/register', (req, res) => {
-  // implement registration
-});
+// importing constan, model, validation(string)
+const constants = require("../config/constants.js");
+const Users = require("../jokes/jokes-model.js");
+const { isValid } = require("../jokes/jokes-service.js");
 
-router.post('/login', (req, res) => {
-  // implement login
-});
+// REGISTER A NEW USER
+router.post("/register", (req, res) => {
+	// implement registration
+	const credentials = req.body;
+
+	if (isValid(credentials)) {
+		const rounds = process.env.BCRYPT_ROUNDS || 8;
+
+		// hashbrown potatoes the password
+		const hash = bcryptjs.hashSync(credentials.password, rounds);
+
+		credentials.password = hash;
+
+		// save the user to the database
+		Users.add(credentials)
+			.then((user) => {
+				res.status(201).json({ data: user });
+			})
+			.catch((error) => {
+				res.status(500).json({ message: error.message });
+			});
+	} else {
+		res.status(400).json({ message: "Please provide username and password" });
+	}
+}); // WORKING
+
+// LOGIN AN EXISTING USER
+router.post("/login", (req, res) => {
+	// implement login
+	const { username, password } = req.body;
+
+	if (isValid(req.body)) {
+		Users.findBy({ username: username })
+			.then(([user]) => {
+				// check the attempted login password against what is stored in the database
+				if (user && bcryptjs.compareSync(password, user.password)) {
+					const token = signToken(user);
+
+					res.status(200).json({ message: "Welcome to our API!", token });
+				} else {
+					res.status(401).json({ message: "Invalid credentials! -inside /login -findBy-" });
+				}
+			})
+			.catch((error) => {
+				res.status(500).json({ message: error.message }, "500 Error Hello!");
+			});
+	} else {
+		res.status(400).json({ message: "Please provide username and password!" });
+	}
+}); // WORKING
+
+function signToken(user) {
+	const payload = {
+		subject: user.id,
+		username: user.username,
+		role: user.role,
+	};
+
+	const secret = constants.jwtSecret;
+
+	const options = {
+		expiresIn: "1d",
+	};
+
+	return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,61 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require("jsonwebtoken");
+const constants = require("../config/constants.js");
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+	const token = req.headers.authorization;
+
+	if (token) {
+		jwt.verify(token, constants.jwtSecret, (error, decodedToken) => {
+			if (error) {
+				// token is not valid or was modified or has expired
+				res.status(401).json({ you: "SHALL NOT PASS!" });
+			} else {
+				// token is good and have access
+				req.decodedToken = decodedToken;
+
+				next();
+			}
+		});
+	} else {
+		res.status(402).json({ message: "Please provide credentials" });
+	}
 };
+
+//custom middleware from previous work for reference
+
+// function validateUserId(req, res, next) {
+// 	// do your magic!
+// 	UserDb.getById(req.params.id)
+// 		.then((result) => {
+// 			if (!result) {
+// 				res.state(404).json({ error: "user ID validation error" });
+// 			} else {
+// 				next();
+// 			}
+// 		})
+// 		.catch((err) => {
+// 			res.status(500).json({ error: "ID validation getById error" });
+// 		});
+// }
+
+// function validateUser(req, res, next) {
+// 	// do your magic!
+// 	if (!req.body.name) {
+// 		res.status(404).json({ error: "create user validation error" });
+// 	} else {
+// 		next();
+// 	}
+// }
+
+// function validatePost(req, res, next) {
+// 	// do your magic!
+// 	let post = req.body;
+// 	if (!post.text) {
+// 		res.status(500).json({ message: "post validation error" });
+// 	} else {
+// 		next();
+// 	}
+// }
